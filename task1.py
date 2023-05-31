51549681c55a84c60523accda1f217c69866508b
ColeCollector <colen827@deltalearns.ca>
2023-05-31 11:02:06 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 """
 Task 1
 Read the map1.txt file and convert to a map that you can navigate a
@@ -9,7 +10,19 @@ w.geometry("925x475")
 w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
-f = open('map1.txt')
+f = open('map1.txt','r')
+x=0
+Walls = []
+
+while True:
+    Walls.append(f.readline())
+    x = x+1
+    if x>15:
+        print(Walls)
+        break
+
+print(len(Walls))
+
 
 
 w.mainloop()
